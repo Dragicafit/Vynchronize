9c9a67b90b8feb3a2916341dffac5e874b830881
Dragicafit <dherbert98@gmail.com>
2020-03-02 17:38:37 +0100

    remove play on load

--- js/declare.js
+++ js/declare.js
@@ -4,5 +4,3 @@ var id = "M7lc1UVf-VE"
 var username = ""
 // Don't allow trailing or leading whitespace!
 var nosymbols = new RegExp("^(([a-zA-Z0-9_-][a-zA-Z0-9 _-]*[a-zA-Z0-9_-])|([a-zA-Z0-9_-]*))$");
-
-jwplayer().play();
